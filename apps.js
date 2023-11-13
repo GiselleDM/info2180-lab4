32262d2d9a9d575cc5d458f1ca8c5f887a5c4d22
gisimclean@gmail.com <138517546+GiselleDM@users.noreply.github.com>
2023-11-12 23:38:15 -0500

    apps updates2

--- apps.js
+++ apps.js
@@ -1,19 +1,40 @@
-// app.js
-$(document).ready(function() {
-    // Listen for click event on the search button
+document.addEventListener('DOMContentLoaded', function() {
     $('#searchButton').click(function() {
-        // Make an AJAX call using jQuery's ajax() method
+        var searchQuery = $('#searchInput').val();
+
+        // Make an AJAX call using jQuery ajax() method
         $.ajax({
             url: 'http://localhost:8081/info2180-lab4/superheroes.php',
             method: 'GET',
-            dataType: 'text',
-            success: function(data) {
-                // Display the list of superheroes in an alert
-                alert(data);
+            data: { query: searchQuery },
+            dataType: 'json',
+            success: function(response) {
+                
+                displayResult(response);
             },
             error: function(error) {
                 console.error('Error fetching superheroes:', error);
             }
         });
     });
+    function displayResult(result) {
+        var resultDiv = $('#result');
+
+        // Clear previous content
+        resultDiv.empty();
+
+        // Check if a superhero is found
+        if (result) {
+            
+            var aliasHeader = $('<h3>').text(result.alias);
+            var nameHeader = $('<h4>').text(result.name);
+            var bioParagraph = $('<p>').text(result.biography);
+
+          
+            resultDiv.append(aliasHeader, nameHeader, bioParagraph);
+        } else {
+            
+            resultDiv.text('Superhero not found');
+        }
+    }
 });
